cda13f36f01f83d65138c482c7601f578e6a0ec0
deluccaopika <ericksrl33@gmail.com>
2026-05-28 16:40:21 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int numero = 5;
+	int numero2 = 144;
+	int numero3 = sqrt(numero2);
+	printf("%i", numero3);
+	
+	return 0;
+}
